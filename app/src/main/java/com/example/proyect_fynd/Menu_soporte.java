86b1bb60d331479bb647ec3956afccfdd71e466d
eJwtzLFuwjAUheF38ZyhMSRgz5WKhKjaigEmy7GvwY2T617HUIp491oo29GRvv/OtDGQ0h57GJlk7aLhYARwpxsu1iv7AksreNPVy7pdaG6EcXVnHKtYJLx4CzTT+1xS05w6nVG9Hhw/bI+iWZN7nzbQfuDuJ33n3ddqeDvW9Jf9Z0k9iZpuEYrrQBNQeZPB56Ht4EcZcxe8UT3cKgJtJdKpjIhVTkASBu1DdUXqXcBrwfAbPUFSemJyzCE8Hv+f8lCu <novoaparra97@gmail.com>
2023-12-06 22:23:20 -0300

    avance mio

--- app/src/main/java/com/example/proyect_fynd/Menu_soporte.java
+++ app/src/main/java/com/example/proyect_fynd/Menu_soporte.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Menu_soporte extends AppCompatActivity {
+    EditText txt_nombre, txt_correo, txt_problema;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_soporte);
+        txt_nombre=findViewById(R.id.txt_soporte_nombre);
+        txt_correo=findViewById(R.id.txt_soporte_correo);
+        txt_problema=findViewById(R.id.txt_soporte_problema);
     }
     public void volver_menu(View v){
         Intent volver_menu=new Intent(Menu_soporte.this, Menu.class);
