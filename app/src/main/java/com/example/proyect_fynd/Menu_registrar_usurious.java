ea4ba65f7d7ef2091d8b14ef508846a63b4382d1
Benjamin <benjaminsalas333@gmail.com>
2023-12-06 22:37:10 -0300

    agrege las instancias para los registros

--- app/src/main/java/com/example/proyect_fynd/Menu_registrar_usurious.java
+++ app/src/main/java/com/example/proyect_fynd/Menu_registrar_usurious.java
@@ -11,12 +11,17 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Menu_registrar_usurious extends AppCompatActivity {
 private FirebaseFirestore db;
+EditText txt_nombre, txt_apellido, txt_correo, txt_clave, sp_genero, txt_nacimiento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_registrar_usuario);
         db = FirebaseFirestore.getInstance();
+        txt_nombre=findViewById(R.id.txt_registro_nombre);
+        txt_apellido=findViewById(R.id.txt_registro_apellido);
+        txt_correo=findViewById(R.id.txt_registro_correo);
+        txt_clave=findViewById(R.id.txt_registro_clave);
     }
     public void volver_menu(View v){
         Intent volver_menu=new Intent(Menu_registrar_usurious.this, Menu.class);
